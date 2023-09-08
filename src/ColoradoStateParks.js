da3a37693bb5b3245f655ae0488ab5d12703039b
mohamed <matassi.mohamed89@gmail.com>
2023-09-08 15:56:34 +0100

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunction from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
+export default function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(RMFunction.trees);
+  RMFunction.wildlife();
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </>
+  );
 }
